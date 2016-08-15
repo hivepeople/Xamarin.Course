8fa65f4e75a1df5dfd302c75b9b13e64d0fa3334
Søren Nguyen Boisen <jargon@users.noreply.github.com>
2016-08-15 20:35:45 +0200

    Added a couple new code samples
     - add: samples for local declarations
     - add: samples for expression statements

---
+++ 03.Intro.CSharp.DotNet/001LocalDeclarations.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xamarin.Course.Intro.CSharp.DotNet
+{
+    namespace Local.Declarations
+    {
+        class Example
+        {
+            public void DeclareThings()
+            {
+                // Constants must always have a value
+                const int theAnswer = 42;
+                const string yes = "Yes";
+
+                // Error: only numbers, bools, strings and
+                // null references allowed to be const
+                //const Guid g = Guid.Empty;
+
+
+                // Variables need not be initialized at declaration
+                int index;
+
+                // Can declare multiple variables of same type
+                int hours = 8, minutes = 14;
+
+                // References can be set to null (empty reference)
+                string answer = null;
+
+                // Invalid: cannot use variable index
+                // before it has been assigned a value
+                //int current = index;
+
+                // Can assign value after declaration
+                index = 1;
+
+                // Now we can use it
+                int current = index;
+            }
+        }
+    }
+}
---
+++ 03.Intro.CSharp.DotNet/002ExpressionStatements.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xamarin.Course.Intro.CSharp.DotNet
+{
+    namespace Expression.Statements
+    {
+        class Example
+        {
+            public int Add(int x, int y)
+            {
+                return x + y;
+            }
+
+            public void F()
+            {
+                // Assignment
+                int x = Add(1, 1);
+
+                // Increment
+                x++;
+
+                // Decrement
+                x--;
+
+                // Method call
+                Add(2, 3);
+
+                // Instantiation
+                new Example();
+
+                // Error: only assignment, call, increment, decrement
+                // and new object expressions can be used as statements
+                //2 + 2;
+            }
+        }
+    }
+}
